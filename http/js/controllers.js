5d6e911826d54a1229ccc6b964175216772a8a39
s <ste.luong@gmail.com>
2016-10-05 01:03:22 +0200

    total in view

--- http/js/controllers.js
+++ http/js/controllers.js
@@ -13,9 +13,18 @@ mainControllers.controller('mainCtrl', ['$scope',
 mainControllers.controller('readCtrl', ['$scope','Transactions',
         function($scope, Transactions) {
             $scope.transactions = [];
+            $scope.totalIncome=0;
+            $scope.totalOutcome=0;
             Transactions.getTransactions().success(function(response){
                 response.forEach(function(transaction){
                 $scope.transactions.push(transaction);
+                if(transaction.transaction_type=="Outcome"){
+                    $scope.totalOutcome=$scope.totalOutcome+parseFloat(transaction.value);
+                }
+                else if(transaction.transaction_type=="Income")
+                    $scope.totalIncome=$scope.totalIncome+parseFloat(transaction.value);
+                else 
+                    console.log("ERROR in transaction type")
                 })
             });
 
